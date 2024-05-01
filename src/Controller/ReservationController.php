f357a8d875115703c0716e4b800b7bbf48f9ad77
sabrina_dr <sabrina.dermeche@yahoo.com>
2024-05-01 17:42:50 +0200

    #

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class ÀProposController extends AbstractController
+class ReservationController extends AbstractController
 {
-    #[Route('//propos', name: 'app__propos')]
+    #[Route('/reservation', name: 'app_reservation')]
     public function index(): Response
     {
-        return $this->render('Àpropos/concept.html.twig', [
-            'controller_name' => 'ÀProposController',
+        return $this->render('reservation/index.html.twig', [
+            'controller_name' => 'ReservationController',
         ]);
     }
 }
