c495e578fb9779b7208b4a50287d724c1d168688
sabrina_dr <sabrina.dermeche@yahoo.com>
2024-05-20 16:49:04 +0200

    modification des page

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ReservationController extends AbstractController
 {
-    #[Route('/reservation', name: 'app_reservation')]
-    public function index(): Response
+    #[Route('/reservations', name: 'app_reservation', methods: ['GET'])]
+    public function index(ReservationRepository $repo): Response
     {
+
+        $reservations= $repo ->findAll();
+
         return $this->render('reservation/réservation.html.twig', [
-            'controller_name' => 'ReservationController',
+            // 'controller_name' => 'ReservationController',
+            'LesReservations' => $reservations
         ]);
     }
+
+    // #[Route('/reservation', name: 'app_reservation', methods: ['GET'])]
+    // public function reservation($id, ReservationRepository $repo): Response
+    // {
+
+    //     $reservation= $repo ->find($id);
+
+    //     return $this->render('reservation/réservation.html.twig', [
+    //         // 'controller_name' => 'ReservationController',
+    //         'LaReservation' => $reservation
+    //     ]);
+    // }
 }
---
+++ src/Entity/Reservation.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservationRepository::class)]
+class Reservation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $mail = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $avatar = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $heur = null;
+
+    #[ORM\Column]
+    private ?int $nombrePersonnes = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): static
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getAvatar(): ?string
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar(string $avatar): static
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getHeur(): ?\DateTimeInterface
+    {
+        return $this->heur;
+    }
+
+    public function setHeur(\DateTimeInterface $heur): static
+    {
+        $this->heur = $heur;
+
+        return $this;
+    }
+
+    public function getNombrePersonnes(): ?int
+    {
+        return $this->nombrePersonnes;
+    }
+
+    public function setNombrePersonnes(int $nombrePersonnes): static
+    {
+        $this->nombrePersonnes = $nombrePersonnes;
+
+        return $this;
+    }
+}
